17f1be574708b7fb6058f3d2c8377b478a30a759
Ashis Mahapatra <Ashis.61091393@ltimindtree.com>
2024-04-30 13:53:40 +0530

    initial

---
+++ src/redux/actions/movieActions.js
@@ -0,0 +1,6 @@
+import { getRequest } from "../../shared/httpRequest";
+import * as types from "./actionTypes";
+
+// Add movie action functions here as needed.  Use the getRequest function imported above to retrieve
+// data from the https://www.omdbapi.com endpoint, either using the apiKey e27098be or creating
+// your own.  Refer to https://www.omdbapi.com/ also for API documentations.
---
+++ src/redux/reducers/movieReducer.js
@@ -0,0 +1,7 @@
+import * as types from "../actions/actionTypes";
+
+const initialState = {
+  // Set up initial state for movies store as needed.
+};
+
+// Define movieReducer function here.
